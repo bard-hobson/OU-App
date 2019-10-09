0e09ddde8cbfccda6981f506ed6f4448e47eaceb
bjknight87 <bjknight87@yahoo.com>
2019-10-08 22:59:00 -0500

    Updates

---
+++ precache-manifest.4c849aa3cc2a69761195ce4e745c1830.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2a7ee49e8f534b90db5",
+    "url": "/OU-App/static/css/main.5d6c0b8f.chunk.css"
+  },
+  {
+    "revision": "e2a7ee49e8f534b90db5",
+    "url": "/OU-App/static/js/main.e2a7ee49.chunk.js"
+  },
+  {
+    "revision": "2ad8966f65a92f999525",
+    "url": "/OU-App/static/js/1.2ad8966f.chunk.js"
+  },
+  {
+    "revision": "4572e0d8658e52f03f48",
+    "url": "/OU-App/static/js/runtime~main.4572e0d8.js"
+  },
+  {
+    "revision": "bc185c9dd0be83041d2763c29206992f",
+    "url": "/OU-App/index.html"
+  }
+];
